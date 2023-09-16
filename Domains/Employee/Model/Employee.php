1935f0d7809fddc879b21dfad2be7504decbc984
araujopedroc <113073167+araujopedroc@users.noreply.github.com>
2023-09-16 20:50:21 -0300

    Update Employee.php

--- Domains/Employee/Model/Employee.php
+++ Domains/Employee/Model/Employee.php
@@ -1,32 +1,29 @@
 <?php
     require("../Functions/ValidateCpf.php");
+    require_once('User.php');
 
-    class Employee{
-        private $fullName;
-        private $email;
-        private $phoneNumber;
+    class Employee extends User
+    {
+        private $salary;
         private $fullAddress;
         private $CPF;
 
-        public function __construct($fullName, $email, $phoneNumber, $fullAddress, $CPF){
-            $this->fullName = $fullName;
-            $this->email = $email;
-            $this->phoneNumber = $phoneNumber;
-            $this->fullAddress = $fullAddress;
-
-            validateCPF($CPF);
-            $this->CPF = $CPF;
-        }
-
-        public function getFullName(){return $this->fullName;}
-        public function getEmail(){return $this->email;}
-        public function getPhoneNumber(){return $this->phoneNumber;}
-        public function getFullAddress(){return $this->fullAddress;}
+        public function __construct($fullName, $email, $phoneNumber, $salary ,$fullAddress ,$CPF) 
+      {
+          parent::__construct($fullName, $email, $phoneNumber); // Chama o construtor da classe Pai
+          $this->salary = $salary;
+          $this->fullAddress = $fullAddress;
+          validateCPF($CPF);
+          $this->CPF = $CPF;
+      }
+      
+        public function getSalary(){return $this->salary;}
+        public function getAddress(){return $this->fullAddress;}
         public function getCPF(){return $this->CPF;}
 
-        public function setFullName($fullName){$this->fullName = $fullName;}
-        public function setEmail($email){$this->email = $email;}
-        public function setPhoneNumber($phoneNumber){$this->phoneNumber = $phoneNumber;}
-        public function setFullAddress($fullAddress){$this->fullAddress = $fullAddress;}
-    }
+        public function setSalary($fullName){$this->salary = $salary;}
+        public function setAddress($email){$this->fullAddress = $fullAddress;}
+        public function setCPF($phoneNumber){$this->CPF = $CPF;}
+      
+    } 
 ?>
