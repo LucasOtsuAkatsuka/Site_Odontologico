e34bbbe14c12c345b7a394514097c8e67f06b09c
ViniciusVsV <viniciusvsv193@gmail.com>
2023-10-01 16:02:24 -0300

    feat: Appointment persist

--- Domains/Appointment/Model/Appointment.php
+++ Domains/Appointment/Model/Appointment.php
@@ -1,14 +1,16 @@
 <?php
-
+    require_once(__DIR__."../../../../Database/persist.php");
     require_once(__DIR__."../../../Dentist/Model/Dentist.php");
     require_once(__DIR__."../../../Patient/Model/Patient.php");
 
-    class Appointment{
+    class Appointment extends persist{
         private $patient;
         private $appointmentDentist;
         private $appoitmentDate;
         private $appoitmentTime;
         private $expectedDuration;
+
+        protected static $local_filename = "Appointment.txt";
 
         public function __construct( $patient, $appointmentDentist, $appoitmentDate, $appoitmentTime, $expectedDuration){
             $this->patient = $patient;
@@ -16,6 +18,10 @@
             $this->appoitmentDate = $appoitmentDate;
             $this->appoitmentTime = $appoitmentTime;
             $this->expectedDuration = $expectedDuration;
+        }
+
+        static public function getFilename(){
+            return get_called_class()::$local_filename;
         }
 
         public function getPatient(){return $this->patient;}
