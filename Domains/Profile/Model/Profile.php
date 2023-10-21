82471de4beb7678fd0d3214e3e95e93eb016494b
Luiz Filipe Perini Giacomin <luiz.p.giacomin@gmail.com>
2023-10-21 13:59:17 -0300

    Fix: Profile.php

--- Domains/Profile/Model/Profile.php
+++ Domains/Profile/Model/Profile.php
@@ -5,8 +5,16 @@
     class Profile extends persist{
         
         protected static $local_filename = "Profile.txt";
+        private $profileType;
         private $permissions = array();
 
+        public function __construct($profileType){
+            $this->profileType = $profileType;
+        }
+        
+        public function getProfileType(){return $this->profileType;}
+        public function setProfileType($profileType){$this->profileType = $profileType;}
+        
         public function getPermissions(){return $this->permissions;}
         public function addPermissions($permission){$this->permissions[] = $permission;}
         
